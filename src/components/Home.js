c7d3031b5db1a62080f3b996606bdecda8e670d0
irfan dayan <engr.irfandayan@gmail.com>
2022-02-14 14:21:46 +0500

    Initials with home component+button compoents

---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+
+import Section from "./Section";
+
+const Container = styled.div`
+  height: 100vh;
+`;
+
+function Home() {
+  return (
+    <Container>
+      <Section />
+    </Container>
+  );
+}
+
+export default Home;
