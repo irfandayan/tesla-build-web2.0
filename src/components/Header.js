1af0b1fc160701b99e2e9d530f1c3af1ca481d30
irfan dayan <engr.irfandayan@gmail.com>
2022-02-14 21:46:54 +0500

    Add redux store

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,12 @@ import styled from "styled-components";
 import MenuIcon from "@mui/icons-material/Menu";
 import CloseIcon from "@mui/icons-material/Close";
 
+import { useSelector } from "react-redux";
+import { selectCars } from "../features/car/carSlice";
+
 function Header() {
   const [burgerStatus, setBurgerStatus] = useState(false);
+  const cars = useSelector(selectCars);
 
   return (
     <Container>
@@ -12,10 +16,12 @@ function Header() {
         <img src="/images/logo.svg" alt="" />
       </a>
       <Menu>
-        <a href="#">Model S</a>
-        <a href="#">Model 3</a>
-        <a href="#">Model X</a>
-        <a href="#">Model Y</a>
+        {cars &&
+          cars.map((car, index) => (
+            <a key={index} href="#">
+              {car}
+            </a>
+          ))}
       </Menu>
 
       <RightMenu>
@@ -29,33 +35,12 @@ function Header() {
           <CustomClose onClick={() => setBurgerStatus(false)} />
         </CloseWrapper>
 
-        <li>
-          <a href="#">Existing Inventory</a>
-        </li>
-        <li>
-          <a href="#">Used Inventory</a>
-        </li>
-        <li>
-          <a href="#">Trade-In</a>
-        </li>
-        <li>
-          <a href="#">CyberTruck</a>
-        </li>
-        <li>
-          <a href="#">Roadaster</a>
-        </li>
-        <li>
-          <a href="#">Existing</a>
-        </li>
-        <li>
-          <a href="#">Existing</a>
-        </li>
-        <li>
-          <a href="#">Existing</a>
-        </li>
-        <li>
-          <a href="#">Existing</a>
-        </li>
+        {cars &&
+          cars.map((car, index) => (
+            <li key={index}>
+              <a href="#">{car}</a>
+            </li>
+          ))}
       </BurgerNav>
     </Container>
   );
@@ -121,6 +106,7 @@ const BurgerNav = styled.div`
   flex-direction: column;
   text-align: start;
   transform: ${(props) => (props.show ? "translateX(0)" : "translateX(100%)")};
+  transition: transform 0.2s;
   li {
     padding: 15px 0;
     border-bottom: 1px solid rgba(0, 0, 0, 0.2);
