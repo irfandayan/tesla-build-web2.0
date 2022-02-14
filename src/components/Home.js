44e4757b55dc64176788b51bf502a2b5c054ed17
irfan dayan <engr.irfandayan@gmail.com>
2022-02-14 15:42:10 +0500

    Add other model section

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,7 +10,59 @@ const Container = styled.div`
 function Home() {
   return (
     <Container>
-      <Section />
+      <Section
+        title="Model S"
+        description="Order Online for Touchless Delivery"
+        leftBtnText="Custom Order"
+        rightBtnText="Existing Inventory"
+        backgroudImag="model-s.jpg"
+      />
+      <Section
+        title="Model Y"
+        description="Order Online for Touchless Delivery"
+        leftBtnText="Custom Order"
+        rightBtnText="Existing Inventory"
+        backgroudImag="model-y.jpg"
+      />
+
+      <Section
+        title="Model 3"
+        description="Order Online for Touchless Delivery"
+        leftBtnText="Custom Order"
+        rightBtnText="Existing Inventory"
+        backgroudImag="model-3.jpg"
+      />
+
+      <Section
+        title="Model X"
+        description="Order Online for Touchless Delivery"
+        leftBtnText="Custom Order"
+        rightBtnText="Existing Inventory"
+        backgroudImag="model-x.jpg"
+      />
+
+      <Section
+        title="Lowest Cost Solar Panels in Amercia"
+        description="Money-Back Guarantee"
+        leftBtnText="Order Now"
+        rightBtnText="Learn More"
+        backgroudImag="solar-panel.jpg"
+      />
+
+      <Section
+        title="Solar for New Roofs"
+        description="Solar Roof Costs Less Than a New Roof Plus Solar Panels "
+        leftBtnText="Custom Order"
+        rightBtnText="Learn More"
+        backgroudImag="solar-roof.jpg"
+      />
+
+      <Section
+        title="Accessories"
+        description=""
+        leftBtnText="Shop Now"
+        backgroudImag="accessories.jpg"
+      />
     </Container>
   );
 }
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,17 +1,23 @@
 import React from "react";
 import styled from "styled-components";
 
-function Section() {
+function Section({
+  title,
+  description,
+  leftBtnText,
+  rightBtnText,
+  backgroudImag,
+}) {
   return (
-    <Wrap>
+    <Wrap bgImage={backgroudImag}>
       <ItemText>
-        <h1>Model S</h1>
-        <p>Order Online for Touchless Delivery</p>
+        <h1>{title}</h1>
+        <p>{description}</p>
       </ItemText>
       <Buttons>
         <ButtonGroup>
-          <LeftButton>Custom Order</LeftButton>
-          <RightButton>Existing Inventory</RightButton>
+          <LeftButton>{leftBtnText}</LeftButton>
+          {rightBtnText && <RightButton>{rightBtnText}</RightButton>}
         </ButtonGroup>
 
         <DownArrow src="/images/down-arrow.svg" />
@@ -33,6 +39,7 @@ const Wrap = styled.div`
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
+  background-image: ${(props) => `url("images/${props.bgImage}")`};
 `;
 
 const ItemText = styled.div`
@@ -43,6 +50,9 @@ const ItemText = styled.div`
 const ButtonGroup = styled.div`
   display: flex;
   margin-bottom: 30px;
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
 `;
 
 const LeftButton = styled.div`
@@ -61,10 +71,13 @@ const LeftButton = styled.div`
   margin: 8px;
 `;
 
-const RightButton = styled(LeftButton)``;
+const RightButton = styled(LeftButton)`
+  background: #fff;
+  opacaity: 0.65;
+  color: #000;
+`;
 
 const DownArrow = styled.img`
-  margin-top: 20px;
   height: 40px;
   animation: animateDown infinite 1.5s;
   overflow-x: hidden;
